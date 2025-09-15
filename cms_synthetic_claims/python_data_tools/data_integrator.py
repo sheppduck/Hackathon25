5672eaa543c693185c9d9d2b347fde095befb62f
robert.adler <robert.adler@naunce.com>
2025-09-15 15:20:23 -0400

    Comments / TODO LIST

--- cms_synthetic_claims/python_data_tools/data_integrator.py
+++ cms_synthetic_claims/python_data_tools/data_integrator.py
@@ -1,5 +1,19 @@
 import sqlite3
 
+
+# Generate a New SQL Lite Table that Eliminates Non-Relevant Fields
+# Based on the "Relevant" Column in the Definitions Table
+# We need to Map the Claims Code Table -> Definitions Code Table as they do not perfectly match in all cases 
+
+
+#TASKS:
+    # Figure out how to get the list of columns from the definitions table where Relevant = 1 and then use that to build the processed claims table -- based on list of col, create new table  -- Robert 
+    # Map the claims code to the definitions code where they do not match perfectly (e.g., Claim ID -> ClaimID, Claim Amount -> Claim_Amount, etc.) -- Robert
+    # Spit out New Sql Table into Databse (With Versioning? ) -- Robert 
+    # Dynamically split cleaned dataset into 20% Test and 80% Train datasets for ML purposes -- Michelle 
+    # Optional Generate and Tag Synthetic Fraud Data -- Manny 
+    # Match in Beneficiary Data to Claims Data for Enriched Dataset (FROM CMS) -- Joel , ED
+
 def create_dynamic_view(db_path, view_name, base_table, definitions_table):
     """
     Create a dynamic SQL view in the SQLite database.
@@ -28,6 +42,12 @@ def create_dynamic_view(db_path, view_name, base_table, definitions_table):
 
     except Exception as e:
         print(f"Error creating view: {e}")
+
+
+
+
+
+
 
 def main():
     db_path = "C:\\Projects\\Hackathon\\Hackathon25\\cms_synthetic_claims\\cms_synthetic_claims.db"
